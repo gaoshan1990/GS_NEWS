1522f0be5abe2f0c314942f88ddd1ac4b93b701b
gaoshan <1328279962@qq.com>
2017-04-27 10:44:44 +0800

    本地仓库添加了测试Git

--- GS_NEWS/GS_NEWS/AppDelegate.m
+++ GS_NEWS/GS_NEWS/AppDelegate.m
@@ -19,6 +19,10 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
+    NSLog(@"这个测试Git");
+    
+    
+    
     self.window =[[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     
     self.window.backgroundColor =[UIColor whiteColor];
